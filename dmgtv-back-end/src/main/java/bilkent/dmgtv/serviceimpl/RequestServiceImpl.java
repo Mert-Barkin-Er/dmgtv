0ad7de159246d8104b330f93425933db88b3042e
Mert Barkın ER <mertbarkn@gmail.com>
2022-05-16 00:37:41 +0300

    Added movie request tables

---
+++ dmgtv-back-end/src/main/java/bilkent/dmgtv/serviceimpl/RequestServiceImpl.java
@@ -0,0 +1,27 @@
+package bilkent.dmgtv.serviceimpl;
+
+import bilkent.dmgtv.db.Request;
+import bilkent.dmgtv.dto.RequestDto;
+import bilkent.dmgtv.repository.RequestRepository;
+import bilkent.dmgtv.service.RequestService;
+import bilkent.dmgtv.serviceimpl.base.BaseServiceImpl;
+import bilkent.dmgtv.serviceimpl.mapper.RequestMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RequestServiceImpl extends BaseServiceImpl<Request, RequestDto> implements RequestService
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(RequestServiceImpl.class);
+
+	private final RequestRepository requestRepository;
+	private final RequestMapper requestMapper;
+
+	public RequestServiceImpl(RequestRepository requestRepository, RequestMapper requestMapper)
+	{
+		super(requestRepository, RequestMapper.INSTANCE);
+		this.requestRepository = requestRepository;
+		this.requestMapper = requestMapper;
+	}
+}
