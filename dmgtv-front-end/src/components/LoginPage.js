5d7c5e4f095eb91703b538958587736fd3802f92
Doruk Kantarcıoğlu <dorukkantarcioglu@gmail.com>
2022-05-15 21:32:06 +0300

    Now we can login

--- dmgtv-front-end/src/components/LoginPage.js
+++ dmgtv-front-end/src/components/LoginPage.js
@@ -1,29 +1,33 @@
 import { Button, TextField } from "@mui/material";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../css/LoginPage.css";
 
 export default function LoginPage() {
     const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
     const navigate = useNavigate();
-
+    
     function handleLogin(e) {
         e.preventDefault();
-        sessionStorage.setItem('username', JSON.stringify(username));
-        if (username === "admin") {
-            navigate("/employee");
-        }
-        else {
+        axios.post("http://localhost:8080/user/login", {
+            username: username,
+            password: password
+        }).then((res) => {
+            sessionStorage.setItem('username', JSON.stringify(username));
             navigate("/profile");
-        }
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 
     return (
         <div className="LoginPage">
             Proceed to log into DMGTV:
             <form noValidate autoComplete="off" onSubmit={(e) => {handleLogin(e);}} style={{marginTop: "10%"}}>
-                <TextField label="Username" onChange={(e) => {setUsername(e.target.value)}}/>
-                <TextField label="Password" type="password"/>
+                <TextField label="Username" onChange={(e) => {setUsername(e.target.value);}}/>
+                <TextField label="Password" type="password" onChange={(e) => {setPassword(e.target.value);}}/>
                 <Button className="LoginPageButton" type="submit" style={{ marginTop: "1.5%" }}>Log In</Button>
             </form>
         </div>
